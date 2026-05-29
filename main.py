8a960cc084a5af601d22c8f4f81bccd48bc5df59
kajiwo1613 <kajiwo1613@gmail.com>
2026-05-29 09:41:24 +0900

    Update main.py

--- main.py
+++ main.py
@@ -27,6 +27,6 @@ async def ask(ctx, *, question):
         )
         await ctx.send(response.text)
     except Exception as e:
-        await ctx.send("すみません、エラーが発生しました。")
+        await ctx.send(f"エラー原因:[e]")
 
 bot.run(DISCORD_TOKEN)
